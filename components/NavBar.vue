a5ce5534f81d5d9041b4eb04ead49a6be1c9ef82
Dre <42796363+Dre-Mylove@users.noreply.github.com>
2022-12-05 19:52:35 -0800

    Add files via upload
    Dre's components

---
+++ components/NavBar.vue
@@ -0,0 +1,83 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">LazyCars</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      ></button>
+      <div class="mx-auto" style="width: 400px"></div>
+      <div class="collapse navbar-collapse" id="navbarScroll">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link to="/HomeView" class="nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">Finance</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" class="nav-link">About</router-link>
+            </li>
+          </ul>
+        </div>
+        <router-view />
+        <span class="border border-white px-2 m-2">
+          <a class="navbar-brand rounded-cirlce" href="#">LogIn</a>
+        </span>
+        <span class="border border-white px-2">
+          <a class="navbar-brand" href="#">Sign Up</a>
+        </span>
+      </div>
+    </div>
+  </nav>
+
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: white;
+}
+
+nav {
+  overflow: hidden;
+  position: fixed; /* Set the navbar to fixed position */
+  top: 0; /* Position the navbar at the top of the page */
+  width: 100%; /* Full width */
+}
+
+#search {
+  display: inline-block;
+  vertical-align: top;
+}
+</style>
